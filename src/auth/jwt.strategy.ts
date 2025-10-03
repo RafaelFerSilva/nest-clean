e2288dd1639e169f788848310e39062bd7a06022
RafaelFerSilva <todosomtodavoz@gmail.com>
2025-10-03 09:07:55 -0300

    feat: use ath guard

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Env } from "src/env";
+import z from "zod";
+
+const tokenSchema = z.object({
+    sub: z.uuid()
+})
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+    constructor(config: ConfigService<Env, true>) {
+        const publicKey = config.get<string>('JWT_PUBLIC_KEY', { infer: true });
+
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: Buffer.from(publicKey, 'base64'),
+            algorithms: ['RS256']
+        })
+    }
+
+    async validate(payload: any) {
+        return tokenSchema.parse(payload);
+    }
+}
